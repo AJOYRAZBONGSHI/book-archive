050be5283dbd2eb64bce5ed706d02b41b6407eed
AJOYRAZBONGSHI <razbongshiajoy111@.com>
2021-09-02 22:48:48 +0600

    done

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ const getResult = () => {
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
       .then((res) => res.json())
-      .then((data) => displayData(data.docs));
+      .then((data) => displayData(data));
   }
 };
 
@@ -42,7 +42,8 @@ const displayError = () => {
 };
 
 // display search data
-const displayData = (books) => {
+const displayData = (booksAll) => {
+  const books = booksAll.docs;
   // spinner display none
   document.getElementById("spinner").style.display = "none";
 
@@ -55,17 +56,17 @@ const displayData = (books) => {
   } else {
     // search items
     const searchItem = document.getElementById("search-item");
-    searchItem.innerHTML = `<h3 class="text-info">Search result found ${books.length}</h3>`;
+    searchItem.innerHTML = `<h3 class="text-info">Search result found ${booksAll.numFound}</h3>`;
     // display  all books
     books.forEach((book) => {
       const div = document.createElement("div");
       div.classList.add = "col";
       div.innerHTML = `
-        <div class="card">
-            <img src="https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg" class="card-img-top" alt="book">
-            <div class="card-body">
+        <div class="card shadow-lg rounded bg-light" onclick="cardClick()">
+            <img src="https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg" class="card-img-top img-fluid rounded-5 p-3" alt="book">
+            <div class="card-body ">
                 <h5 class="card-title">Book name: ${book?.title}</h5>
-                <h5 class="card-title">Author name: ${book?.author_name}</h5>
+                <h5 class="card-title">Author name: ${book.author_name}</h5>
                 <h5 class="card-title">Publisher date: ${book?.first_publish_year}</h5>
             </div>
         </div>
